e738e02b7e77e0e4f0639baf1fd82afd7e7738e7
EmilianoMena <70341436+EmilianoMena@users.noreply.github.com>
2024-02-12 20:23:45 -0600

    Second Changes
    Define the punctuation model based on FICO model and Altman Z-score

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 # On this file the databases will be loaded, on this project we have only one database that is on a csv
 # file named 'train.csv'
 
-# Libraries and dependencies
+## Libraries and dependencies
 import pandas as pd
 
-# Load the train database
+## Load the train database
 df = pd.read_csv('files/train.csv',low_memory=False)
--- functions.py
+++ functions.py
@@ -1,12 +1,13 @@
 # This file contains the functions created to do certain process or operations
 
-# Libraries and dependencies
+## Libraries and dependencies
 import pandas as pd
 import numpy as np
 import string
 import data as d
 
-def analyse_data(data):
+## Data cleaning functions
+def analyse_data(data): 
     df = pd.DataFrame({
         'Variable':list(data.columns.values),
         'Type':list(data.dtypes),
@@ -55,11 +56,23 @@ def only_numbers(x):
 def numerical_values(data):
     return [float(only_numbers(x)) for x in [xi for xi in null_cero_values(data)]]
 
-# Functions for every variable and to asign a final score   
+## Punctuation functions
+### FICO model
 f1 = lambda x: 200 if x <= 3 else 150 if x <= 10 else 100 if x <= 30 else 75 if x <= 60 else 50 if x <= 90 else 25
 f2 = lambda x: 150 if x <= 1 else 120 if x <= 7 else 90 if x <= 13 else 60 if x <= 19 else 30
 f3 = lambda x: 300 if x <= 500 else 275 if x <= 1000 else 250 if x <= 1500 else 150 if x <= 2000 else 75 if x <= 2500 else 50 
 f4 = lambda x: 15 if x <= 24 else 30 if x <= 48 else 45 if x <= 72 else 60 if x <= 96 else 85 if x <=120 else 100
 f5 = lambda x: 150 if x == 'Good' else 75 if x == 'Standard' else 25
 f6 = lambda x: 100 if x <= 2 else 85 if x <= 6 else 60 if x <= 10 else 45 if x <= 14 else 30
-f7 = lambda x: 'Poor' if x<600 else 'Standard' if x<800 else 'Good'
+f7 = lambda x: 50 if x <= 50 else 0
+f8 = lambda x: 50 if x<= 30 else 0
+### Altman Z-score
+fx1 = lambda x,y,z : [xi / (yi + zi) for xi,yi,zi in zip(x,y,z)]
+fx2 = lambda x,y,z : [xi / (yi + zi) for xi,yi,zi in zip(x,y,z)]
+fx3 = lambda x,y,z : [xi / (yi + zi) for xi,yi,zi in zip(x,y,z)]
+fx4 = lambda x,y : [xi/ 12 / zi for xi,yi,zi in zip(x,y)]
+fx5 = lambda x,y,z : [xi / (yi + zi) for xi,yi,zi in zip(x,y,z)]
+fz_score = lambda x1,x2,x3,x4,x5 : 1.2*x1 + 1.4*x2 + 3.3*x3 + 0.6*x4 + x5
+alt_score = lambda x: 1000 if x > 3 else 650 if x > 1.8 else 300
+### Final Score
+final_score = lambda x: 'Poor' if x < 600 else 'Standard' if x < 800 else 'Good' 
